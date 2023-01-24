1b06549721fb635b83774d5b4a7c30152d62505f
Jason Chen <californiaroad1@gmail.com>
2023-01-23 17:45:49 -0800

    Revert "limelight"
    This reverts commit d18c2c4c24757d5cc6865461af27b86dc09d1c21.

---
+++ src/main/java/frc/robot/commands/Limelight.java
@@ -0,0 +1,18 @@
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.networktables.NetworkTable;
+import edu.wpi.first.networktables.NetworkTableEntry;
+import edu.wpi.first.networktables.NetworkTableInstance;
+
+public class Limelight {
+    NetworkTable table;
+    NetworkTableEntry tx;
+    NetworkTableEntry ty;
+    NetworkTableEntry ta;
+
+    public Limelight() {
+        table = NetworkTableInstance.getDefault().getTable("limelight");
+        tx = table.getEntry("tx");
+        ty = table.getEntry("ty");
+        ta = table.getEntry("ta");
+    }
+}
