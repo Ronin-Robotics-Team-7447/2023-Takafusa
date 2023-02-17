287ef13ad50566eabde3dc13be70769ea7bfd554
Junyi Zhong <zhongjunyi.johnny@gmail.com>
2023-02-17 14:02:02 -0800

    test

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -30,7 +30,7 @@ public class Robot extends TimedRobot {
 
   /**
    * This function is run when the robot is first started up and should be used for any
-   * initialization code.
+   * initialization code. a
    */
   @Override
   public void robotInit() {
